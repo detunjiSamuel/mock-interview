cc6b96e7a8e294074303eb7ba22a1c9eb84812c9
Adetunji Samuel <samadetunji01@gmail.com>
2023-06-28 01:16:59 +0100

    add questions template

--- interview-mock-client/src/app/layout.tsx
+++ interview-mock-client/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,7 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+
+     <Navbar/>    
+
+
+       {children}   
+
+       <Footer/>
+
+       </body>
     </html>
+
   )
 }
